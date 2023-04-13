80c0cf73902a9629462a3c51596a0beed876b181
TonyJiashiChen <tonychen9661@gmail.com>
2023-04-13 16:09:38 +1000

    post

--- Cloudiod_server.py
+++ Cloudiod_server.py
@@ -8,6 +8,9 @@ class Cloudiod(Resource):
     def get(self):
         return {"data": "Hello"}
 
+    def post(self):
+        return {"data": "posted"}
+
 api.add_resource(Cloudiod, "/")
 
 if __name__ == "__main__":
